dd42a9aedc2ce51c08651c65b1c78c6374bc3f43
trisimix <trisimix@protonmail.com>
2019-02-15 14:08:22 -0800

    Create showshell.sh

---
+++ showshell.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+flag=0
+serialPort="/dev/ttyUSB0"
+
+trap programExit SIGINT SIGKILL SIGTERM
+
+gcc -o port_open port_open.c
+./port_open $serialPort &
+subppid=$!
+sleep 0.1
+
+function programExit()
+{
+	kill $subppid
+	exit
+}
+
+declare -i length
+isbusy=0
+
+while [ 1 ]
+do
+  read command
+  returned=`command`
+  
+  todo:
+  Split multiple lines into many, parse out stderr and instead make an error line, then write each line based on length. Blegh :(
+  
+done
+
+
+writeData () {
+	echo -ne "\006" > $serialPort
+	str=${1//\\}
+	length=$((${#str} + 48))
+	echo -ne $(printf \\$(printf '%03o' $length)) > $serialPort
+	while [ "$isbusy" != 6 ]
+	do
+		isbusy=$(head -c 1 $serialPort)
+	done
+	echo -ne "$1" > $serialPort
+	isbusy=0
+}
+
+writeData "\ec"
+writeData "Hello ODROID-SHOW"
+
+programExit
